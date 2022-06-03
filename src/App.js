a03ebf0e90e24d9b95f81638cde090d19fd6a303
Pranav <pranavmagare2020@gmail.com>
2022-06-03 13:24:38 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
-import React, { useEffect, useState } from "react";
-import Cart from './Components/Cart/Cart'
-import Navbar from './Components/Navbar/Navbar'
-import ProductsList from "./Components/ProductsList/ProductsList";
+import React from "react";
+import Container from "./Container";
 
 const App = () => {
-
-  const [show, setShow] = useState(true);
-  const [cart, setCart] = useState([]);
-  
-  const handleClick = (item) => {
-    if (cart.indexOf(item) !== -1) return;
-    setCart([...cart, item]);
-  };
-
-  useEffect(() => {
-    let data = window.localStorage.getItem("cart");
-    setCart(JSON.parse(data));
-  }, [setCart]);
-
   return (
     <>
-      <Navbar setShow={setShow} size={cart.length} />
-      
-      {show ? (
-        <ProductsList handleClick={handleClick} />
-      ) : (
-        <Cart cart={cart} setCart={setCart} />
-      )}
+      <Container />
     </>
   );
 };
---
+++ src/Container.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import Cart from './Components/Cart/Cart'
+import Navbar from './Components/Navbar/Navbar'
+import ProductsList from "./Components/ProductsList/ProductsList";
+
+const App = () => {
+
+  const [show, setShow] = useState(true);
+  const [cart, setCart] = useState([]);
+  
+  const handleClick = (item) => {
+    if (cart.indexOf(item) !== -1) return;
+    setCart([...cart, item]);
+  };
+
+  useEffect(() => {
+    let data = window.localStorage.getItem("cart");
+    setCart(JSON.parse(data));
+  }, [setCart]);
+
+  return (
+    <>
+      <Navbar setShow={setShow} size={cart.length} />
+      
+      {show ? (
+        <ProductsList handleClick={handleClick} />
+      ) : (
+        <Cart cart={cart} setCart={setCart} />
+      )}
+    </>
+  );
+};
+
+export default App;
